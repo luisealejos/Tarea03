8432296ea496128883448bcb2812eb9bfb0d35e6
Luis Eduardo Alejos <luisealejos@gmail.com>
2014-04-18 06:29:27 -0600

    Tarea03
    Tarea 3 de Android para SP1

---
+++ src/com/example/tarea03/fragments/StoreMapFragment.java
@@ -0,0 +1,20 @@
+package com.example.tarea03.fragments;
+
+import com.example.tarea03.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StoreMapFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_store_map, container, false);
+	}
+	
+}
